1eb756157ee0733b0e0707373e33385ba5952b22
k.kiseleva <k.kiseleva@fact.digital>
2025-02-02 01:50:58 +0500

    Update models

--- app/Models/Courier.php
+++ app/Models/Courier.php
@@ -2,18 +2,48 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Illuminate\Database\Eloquent\Model;
 
 class Courier extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['courier_type', 'regions', 'working_hours'];
+    protected $fillable = [
+        'courier_type',
+        'regions',
+        'working_hours',
+    ];
 
     protected $casts = [
         'regions' => 'array',
         'working_hours' => 'array',
     ];
+
+    /**
+     * Проверка, доступен ли курьер для работы в заданном регионе и в указанное время
+     */
+    public function isAvailableForRegionAndTime($region, $time)
+    {
+        if (!in_array($region, $this->regions)) {
+            return false;
+        }
+
+        foreach ($this->working_hours as $hours) {
+            if ($this->isTimeWithinWorkingHours($hours, $time)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Проверка, попадает ли время в рабочие часы курьера
+     */
+    private function isTimeWithinWorkingHours($workingHours, $time)
+    {
+        list($start, $end) = explode('-', $workingHours);
+        return ($time >= $start && $time <= $end);
+    }
 }
